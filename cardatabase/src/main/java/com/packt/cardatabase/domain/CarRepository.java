222443d19fad7b87dcd0d8728faa622604b4a270
Hyunjun-Ko <85072422+Hyunjun-Ko@users.noreply.github.com>
2024-09-06 09:37:15 +0900

    add car, owner entities and repositories
    car has a many-to-one relationship with owner.

---
+++ cardatabase/src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -0,0 +1,24 @@
+package com.packt.cardatabase.domain;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CarRepository extends CrudRepository<Car, Long> {
+    @Query("select c from Car c where c.brand = ?1")
+    List<Car> findByBrand(String brand);
+
+    @Query("select c from Car c where c.brand like %?1")
+    List<Car> findByBrandEndsWith(String brand);
+
+    List<Car> findByColor(String color);
+
+    List<Car> findByYear(int year);
+
+    List<Car> findByBrandAndModel(String brand, String model);
+
+    List<Car> findByBrandOrColor(String brand, String color);
+
+    List<Car> findByBrandOrderByYearAsc(String brand);
+}
---
+++ cardatabase/src/main/java/com/packt/cardatabase/domain/Owner.java
@@ -0,0 +1,68 @@
+package com.packt.cardatabase.domain;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+public class Owner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long ownerid;
+    private String firstname, lastname;
+
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+    private List<Car> cars;
+
+    /*
+    @ManyToMany(cascade = CascadeType.PERSIST)
+    @JoinTable(name="car_owner",
+        joinColumns = { @JoinColumn(name="ownerid") },
+        inverseJoinColumns = { @JoinColumn(name="`id`")})
+    private Set<Car> cars = new HashSet<Car>();
+     */
+    public Owner() {}
+
+    public Owner(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public long getOwnerid() {
+        return ownerid;
+    }
+
+    public void setOwnerid(long ownerid) {
+        this.ownerid = ownerid;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+}
