4cb66b79d1fb69f0b4b4fa00dc792bea47a505af
Hyunjun-Ko <85072422+Hyunjun-Ko@users.noreply.github.com>
2024-09-29 23:34:46 +0900

    add a page to show list of cars.
    edited app.js to configure a default theme.
    added a Carlist component to show the list of cars.
    added a file constants.js to add constants such as SERVER_URL.

--- cardatabase/src/main/java/com/packt/cardatabase/SecurityConfig.java
+++ cardatabase/src/main/java/com/packt/cardatabase/SecurityConfig.java
@@ -87,7 +87,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .csrf(Customizer.withDefaults())
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests((authorizeRequests) ->
                         authorizeRequests.anyRequest().permitAll());
         /*
