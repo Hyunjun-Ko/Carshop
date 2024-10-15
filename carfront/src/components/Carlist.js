d15262a330dcce9fa618a4f097e056d79ce3bc36
Hyunjun-Ko <85072422+Hyunjun-Ko@users.noreply.github.com>
2024-10-15 19:52:05 +0900

    add a create feature for Carlist.
    added a component AddCar which is a MUI Dialog to
    prompt the user of new car details and use addCar function
    thats passed as a props to save the new car.
    added the addCar function to Carlist.js to send POST request for
    newly created car.

--- carfront/src/components/Carlist.js
+++ carfront/src/components/Carlist.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { SERVER_URL } from "../constants";
 import { DataGrid } from "@mui/x-data-grid";
 import Snackbar from "@mui/material/Snackbar";
+import AddCar from "./AddCar";
 
 function Carlist() {
   const [cars, setCars] = useState([]);
@@ -49,21 +50,40 @@ function Carlist() {
     }
   };
 
+  const addCar = (car) => {
+    fetch(SERVER_URL + "api/cars", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(car),
+    })
+      .then((response) => {
+        if (response.ok) {
+          fetchCars();
+        } else {
+          alert("Something went wrong!");
+        }
+      })
+      .catch((err) => console.error(err));
+  };
+
   return (
-    <div style={{ height: 500, width: "100%" }}>
-      <DataGrid
-        rows={cars}
-        columns={columns}
-        disableSelectionOnClick={true}
-        getRowId={(row) => row._links.self.href}
-      />
-      <Snackbar
-        open={open}
-        autoHideDuration={2000}
-        onClose={() => setOpen(false)}
-        message="Car deleted"
-      />
-    </div>
+    <React.Fragment>
+      <AddCar addCar={addCar} />
+      <div style={{ height: 500, width: "100%" }}>
+        <DataGrid
+          rows={cars}
+          columns={columns}
+          disableSelectionOnClick={true}
+          getRowId={(row) => row._links.self.href}
+        />
+        <Snackbar
+          open={open}
+          autoHideDuration={2000}
+          onClose={() => setOpen(false)}
+          message="Car deleted"
+        />
+      </div>
+    </React.Fragment>
   );
   /*
   return (
